a229a3d7e8af84d3bc14c9e692814dce21895215
papettoTV <aegfrompsbt@gmail.com>
2020-08-28 17:35:17 +0900

    フォームとプラスボタン表示

---
+++ components/FormInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <b-form inline>
+      <label>変わったこと</label>
+      <b-input
+        id="item"
+        class="mb-2 mr-sm-2 mb-sm-0"
+        placeholder="例）通勤時間"
+      ></b-input>
+      <label>変わった時間</label>
+      <b-input id="change-time" placeholder="例）-1.5"></b-input>
+      <b-icon-plus-circle></b-icon-plus-circle>
+    </b-form>
+    <b-button variant="primary">Save</b-button>
+  </div>
+</template>
+
+<script>
+import { BIconPlusCircle } from 'bootstrap-vue'
+
+export default {
+  components: {
+    BIconPlusCircle,
+  },
+}
+</script>
